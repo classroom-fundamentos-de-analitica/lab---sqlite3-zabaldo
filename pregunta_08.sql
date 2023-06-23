b08645cb81d1b1dfc4aa533da3781b447f803652
david <david@debian-BULLSEYE-live-builder-AMD64>
2023-06-23 16:02:00 -0500

    r

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,4 +41,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT YEAR(c23) AS YEAR, AVG(c21) FROM tbl2 GROUP BY YEAR(c23);
+SELECT YEAR(c23), AVG(c21) FROM tbl2 GROUP BY YEAR(c23);
